8d855b8e4f1b6b4c277398ba437e5d53e15e5f33
carlromeo <carlromeo.dogbey@gmail.com>
2019-09-27 11:57:06 -0400

    ready

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -7,10 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String home() {return "home";}
-    @RequestMapping("about")
+
+    @RequestMapping("/about")
     public String aboutPage() {return "about";}
-    @RequestMapping("authors")
+
+    @RequestMapping("/authors")
     public String authorsPage() {return "authors";}
+
+    @RequestMapping("/blogEntry1")
+    public String blogEntry1Page() {return "blogEntry1";}
+
+    @RequestMapping("/blogEntry2")
+    public String blogEntry2Page() {return "blogEntry2";}
+
+    @RequestMapping("/blogEntry3")
+    public String blogEntry3Page() {return "blogEntry3";}
+
+    @RequestMapping("/blogEntry4")
+    public String blogEntry4Page() {return "blogEntry4";}
+
+    @RequestMapping("/blogEntry5")
+    public String blogEntry5Page() {return "blogEntry5";}
+
+    @RequestMapping("/blogEntry6")
+    public String blogEntry6Page() {return "blogEntry6";}
+
+
+
 
 }
 
